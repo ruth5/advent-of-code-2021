6134ee120a54ef5ee762b6cc21c26ecfcc07d529
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2022-01-05 04:21:02 +0000

    working soluton to part 1

--- advent-9.py
+++ advent-9.py
@@ -7,9 +7,11 @@ def open_and_parse(filename):
         stripped_line = list(line.rstrip())
         parsed_list.append(stripped_line)
         # i = i + 1
+    parsed_list = list(map(int, parsed_list))
     return parsed_list
 
 parsed_list = (open_and_parse('advent-input.txt'))
+# print(parsed_list)
 print(len(parsed_list))
 print(len(parsed_list[0]))
 
@@ -20,7 +22,8 @@ for i in range(len(parsed_list)):
     #each element in that row
     for j in range(len(parsed_list[0])):
         element = int(parsed_list[i][j])
-        min_ = element
+        # min_ = element
+        min_ = 10
         if (i-1) > -1:
             up = int(parsed_list[i-1][j])
             min_ = min(min_, up)
@@ -33,7 +36,8 @@ for i in range(len(parsed_list)):
         if (j + 1) < len(parsed_list[0]):
             right = int(parsed_list[i][j+1])
             min_ = min(min_, right)
-        if min_ == element:
+        # if min_ == element:
+        if element < min_:
             low_points.append(element)
 sum_ = 0
 for point in low_points:
@@ -46,4 +50,3 @@ print(low_points)
 
 
 
-# if there is an up, down, left, right location
